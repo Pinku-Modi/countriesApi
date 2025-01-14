1048e8c757bf331133236bf2289cc719082fae9f
Pinku-Modi <pinku@sigilotech.com>
2025-01-14 10:27:21 +0530

    added country description page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import HomePage from "./pages/HomePage.jsx"
+import CountryDescription from "./pages/CountryDescription.jsx"
 
 
 export default function countriesApiRoutes(){
   return (
     <Routes>
       <Route path="/" element={<HomePage />} /> 
+      <Route path="/:id" element={<CountryDescription />} /> 
     </Routes>
   )
 }
--- src/components/countriesCard.jsx
+++ src/components/countriesCard.jsx
@@ -1,4 +1,8 @@
+import { useNavigate } from "react-router-dom";
+
 export default function CardCountries(props) {
+    const navigate = useNavigate()
+
     return (
         <div className="bg-white rounded-lg shadow-md overflow-hidden mx-auto my-4 max-w-xs h-[300px]">
             <div className="relative w-full h-[120px]">
@@ -10,10 +14,10 @@ export default function CardCountries(props) {
             </div>
 
             <div className="p-4 h-[180px] overflow-hidden">
-                <p className="font-semibold text-lg truncate">{props.name}</p>
-                <p className="text-sm text-gray-600">Population: {props.population.toLocaleString()}</p>
-                <p className="text-sm text-gray-600">Region: {props.region}</p>
-                <p className="text-sm text-gray-600">Capital: {props.capital}</p>
+                <p className="font-semibold text-lg truncate cursor-pointer hover:underline mb-3" onClick={()=>navigate("/:")}>{props.name}</p>
+                <p className="text-sm text-gray-900 "><span className="font-semibold">Population:</span> {props.population.toLocaleString()}</p>
+                <p className="text-sm text-gray-900"><span className="font-semibold">Region:</span> {props.region}</p>
+                <p className="text-sm text-gray-900"><span className="font-semibold">Capital:</span> {props.capital}</p>
             </div>
         </div>
     );
