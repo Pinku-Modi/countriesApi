6a85291dba1d2560ceb2a048bc14174c9ee310e4
Pinku-Modi <pinku@sigilotech.com>
2025-01-14 18:33:50 +0530

    Navbar modification of theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
-
+import darkImage from "../assets/moon-svgrepo-com.svg"
 
 export default function Navbar(){
     return (
         <div className="flex justify-between content-between px-12 py-5" style={{ backgroundColor: 'rgb(255, 255, 255, 1)' }}>
             <p className="font-bold text-xl">Where in the world?</p>
-            <div>
-                <p>Dark Mode</p>
+            <div className="flex justify-evenly content-center items-center gap-2">
+                <img src={darkImage} alt="dark.img" className="h-4 cursor-pointer" />
+                <p className="font-bold">Dark Mode</p>
             </div>
         </div>
     )
