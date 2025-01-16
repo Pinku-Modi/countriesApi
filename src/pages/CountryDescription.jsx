3fed264f34c49c3ca9da9c47e5edbc131da299b5
Pinku-Modi <pinku@sigilotech.com>
2025-01-16 17:35:01 +0530

    experimental codes-> in countryDescription.jsx the actual code is been commented out and inserted a container, Note: View all the conflicts before merge

--- src/pages/CountryDescription.jsx
+++ src/pages/CountryDescription.jsx
@@ -1,10 +1,16 @@
 import { useNavigate, useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
 import Navbar from "../components/Navbar.jsx";
 import backBlackImage from "../assets/left-arrow-Black.svg";
 import backWhiteImage from "../assets/left-arrow-White.svg";
+
+
+// materialUI import
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 export default function CountryDescription() {
     const { name } = useParams();
@@ -34,11 +40,11 @@ export default function CountryDescription() {
             <Navbar />
             <div>
                 <div className={`flex h-[36px] w-[90px] justify-center items-center my-3 cursor-pointer ml-4 p-2 gap-2 rounded-md ${classButton}`} onClick={() => navigate("/")}>
-                    { themeFromStore === "dark" ? <img src={backWhiteImage} alt="Back" className="h-full" /> : <img src={backBlackImage} alt="Back" className="h-full" /> }
+                    {themeFromStore === "dark" ? <img src={backWhiteImage} alt="Back" className="h-full" /> : <img src={backBlackImage} alt="Back" className="h-full" />}
                     <button className="font-semibold">Back</button>
                 </div>
-
                 {country ? (
+                    /* 
                     <div className="grid grid-cols-1 justify-center sm:flex sm:justify-center md:flex md:justify-evenly md:flex-wrap lg:flex lg:justify-evenly lg:flex-wrap mt-6 sm:mt-10 md:mt-13 lg:mt-16 w-full">
                         <img src={country.flags.png} alt={country.name.common} className="w-full sm:w-2/4 md:w-1/3 lg:w-1/3 px-4 max-w-full" />
 
@@ -62,11 +68,23 @@ export default function CountryDescription() {
                                 <span className="font-semibold text-base md:text-lg lg:text-lg">Border Countries:</span> {country.borders ? country.borders.join(', ') : 'None'}
                             </div>
                         </div>
+                    </div> */
+
+                    //  materialUI implementation
+                    < div >
+                    < Fragment >
+                        <CssBaseline />
+                        <Container maxWidth="sm">
+                            <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+                        </Container>
+                    </Fragment>
                     </div>
-                ) : (
-                    <p>Loading...</p>
+                
+
+            ) : (
+            <p>Loading...</p>
                 )}
-            </div>
         </div>
+        </div >
     );
 }
