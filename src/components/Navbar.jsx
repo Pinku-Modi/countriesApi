a0aa587b34333c6aed1b077577d4da987dd72879
Pinku-Modi <pinku@sigilotech.com>
2025-01-20 18:58:14 +0530

    navbar modification

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,18 +18,26 @@ export default function Navbar() {
     }
 
     return (
-        <div className={`flex justify-between content-between px-4 sm:px-8 md:px-10 lg:px-12 py-5 ${themeFrromStore === "dark" ? "bg-black text-white" : "bg-white text-black"
-            }`}>
-            <p className="font-bold text-lg sm:text-lg md:text-xl lg:text-2xl">Where in the world?</p>
-            <div className="flex justify-evenly items-center gap-2 h-auto w-max">
-                {
-                    (themeFrromStore === "dark") ?
-                        <img s
-                        rc={darkImage} alt="dark.img" className="h-4 sm:h-5 md:h-6 lg:h-8 cursor-pointer" onClick={handleChangeTheme} /> :
-                        <img src={lightImage} alt="light.img" className="h-4 sm:h-5 md:h-6 lg:h-8 cursor-pointer" onClick={handleChangeTheme} />
-                }
-                <p className="font-bold text-lg sm:text-lg md:text-xl lg:text-2xl lg:gap-4">{themeFrromStore}</p>
-            </div>
-        </div>
+        // <div className={`flex justify-between content-between px-4 sm:px-8 md:px-10 lg:px-12 py-5 ${themeFrromStore === "dark" ? "bg-black text-white" : "bg-white text-black"
+        //     }`}>
+        //     <p className="font-bold text-lg sm:text-lg md:text-xl lg:text-2xl">Where in the world?</p>
+        //     <div className="flex justify-evenly items-center gap-2 h-auto w-max">
+        //         {
+        //             (themeFrromStore === "dark") ?
+        //                 <img s
+        //                 rc={darkImage} alt="dark.img" className="h-4 sm:h-5 md:h-6 lg:h-8 cursor-pointer" onClick={handleChangeTheme} /> :
+        //                 <img src={lightImage} alt="light.img" className="h-4 sm:h-5 md:h-6 lg:h-8 cursor-pointer" onClick={handleChangeTheme} />
+        //         }
+        //         <p className="font-bold text-lg sm:text-lg md:text-xl lg:text-2xl lg:gap-4">{themeFrromStore}</p>
+        //     </div>
+        // </div>
+
+        <AppBar position="static" >
+            <Typography>
+                Where in the world?
+            </Typography>
+            
+
+        </AppBar>
     )
 }
